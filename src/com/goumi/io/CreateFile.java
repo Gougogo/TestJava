1eefece4101c839eebc26aeefef73ccbe8069b9e
Gougogo <1572158030@qq.com>
2023-08-11 15:23:55 +0800

    commit

---
+++ src/com/goumi/io/CreateFile.java
@@ -0,0 +1,39 @@
+package com.goumi.io;
+
+import net.mindview.atunit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+/**
+ * @version 1.0
+ * @auther GouMi
+ * 单个字节的读取
+ */
+public class CreateFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("d:\\newasd.txt");
+        FileInputStream fileInputStream = null;
+        try {
+            file.createNewFile();
+            fileInputStream = new FileInputStream(file);
+
+            int readData = 0;
+            byte b[]= new byte[5];
+            while ((readData = fileInputStream.read(b)) != -1){
+                System.out.print(readData);
+                String s = new String(b);
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fileInputStream.close();
+        }
+    }
+    @Test
+    public void writeFile(){
+
+    }
+}

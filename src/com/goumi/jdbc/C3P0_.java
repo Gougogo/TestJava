c2fbc7545c2a6117bfdf92f415b8487a85733fe7
Gougogo <1572158030@qq.com>
2023-09-18 23:25:14 +0800

    commit

--- src/com/goumi/jdbc/C3P0_.java
+++ src/com/goumi/jdbc/C3P0_.java
@@ -2,6 +2,7 @@ package com.goumi.jdbc;
 
 import com.goumi.utils.JDBCUtils;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.junit.jupiter.api.Test;
 
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
@@ -12,6 +13,8 @@ import java.sql.SQLException;
  * @auther GouMi
  */
 public class C3P0_ {
+
+    @Test
     public void testC3P0() throws PropertyVetoException, SQLException {
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
         comboPooledDataSource.setDriverClass(JDBCUtils.getDriver());
@@ -22,12 +25,23 @@ public class C3P0_ {
         comboPooledDataSource.setInitialPoolSize(10);
         //最大连接数
         comboPooledDataSource.setMaxPoolSize(50);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 5000; i++) {
+            Connection connection = comboPooledDataSource.getConnection();
+            //connection.close();
+        }
 
-        comboPooledDataSource.getConnection();
-
+        long end = System.currentTimeMillis();
+        System.out.println("c3p0 need " + (end-start));
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws SQLException {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 5000; i++) {
+            Connection connection = JDBCUtils.getConnection();
+            JDBCUtils.close(null, null, connection);
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("traditional need " + (end-start));
     }
 }

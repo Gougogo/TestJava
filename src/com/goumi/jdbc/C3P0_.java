a62b22881ab1f212a3f2339353043ccf985ebcb9
Gougogo <1572158030@qq.com>
2023-09-19 10:35:36 +0800

    commit

--- src/com/goumi/jdbc/C3P0_.java
+++ src/com/goumi/jdbc/C3P0_.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -16,6 +18,7 @@ public class C3P0_ {
 
     @Test
     public void testC3P0() throws PropertyVetoException, SQLException {
+        //初始化连接池
         ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
         comboPooledDataSource.setDriverClass(JDBCUtils.getDriver());
         comboPooledDataSource.setJdbcUrl(JDBCUtils.getUrl());
@@ -28,11 +31,25 @@ public class C3P0_ {
         long start = System.currentTimeMillis();
         for (int i = 0; i < 5000; i++) {
             Connection connection = comboPooledDataSource.getConnection();
-            //connection.close();
+            //数据库操作
+            PreparedStatement show_tables = connection.prepareStatement("show tables");
+            ResultSet resultSet = show_tables.executeQuery();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
+            connection.close();
         }
 
         long end = System.currentTimeMillis();
         System.out.println("c3p0 need " + (end-start));
+    }
+
+    @Test
+    public void testC3p0() throws SQLException {
+        ComboPooledDataSource goumi_c3p0 = new ComboPooledDataSource("goumi_c3p0");
+        Connection connection = goumi_c3p0.getConnection();
+        System.out.println("connect success");
+        connection.close();
     }
 
     public static void main(String[] args) throws SQLException {
---
+++ src/com/goumi/jdbc/Druid_.java
@@ -0,0 +1,34 @@
+package com.goumi.jdbc;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+/**
+ * @version 1.0
+ * @auther GouMi
+ */
+public class Druid_ {
+    @Test
+    public void testDruid() throws Exception {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src\\druid.properties"));
+
+        DataSource dataSource = DruidDataSourceFactory.createDataSource(properties);
+        Connection connection = dataSource.getConnection();
+        PreparedStatement show_tables = connection.prepareStatement("show tables");
+        ResultSet resultSet = show_tables.executeQuery();
+        while (resultSet.next()){
+            System.out.println(resultSet.getString(1));
+        }
+    }
+}

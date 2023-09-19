adb52d84daeaa8fc5fd2d96767a68192fc30ef25
Gougogo <1572158030@qq.com>
2023-09-19 18:14:45 +0800

    commit

--- src/com/goumi/jdbc/dao_/test/TestDao.java
+++ src/com/goumi/jdbc/dao_/test/TestDao.java
@@ -2,6 +2,7 @@ package com.goumi.jdbc.dao_.test;
 
 import com.goumi.jdbc.dao_.dao.ActorDao;
 import com.goumi.jdbc.dao_.domin.Actor;
+import org.junit.jupiter.api.Test;
 
 import java.util.Iterator;
 import java.util.List;
@@ -11,9 +12,10 @@ import java.util.List;
  * @auther GouMi
  */
 public class TestDao {
+    @Test
     public void testActorDao(){
         ActorDao actorDao = new ActorDao();
-        List<Actor> show_tables = actorDao.queryMulti("show tables", Actor.class, null);
+        List<Actor> show_tables = actorDao.queryMulti("select * from actor", Actor.class, null);
         Iterator<Actor> iterator = show_tables.iterator();
         while (iterator.hasNext()){
             Actor next = iterator.next();

53b4580a7aaaa9beb860932a4f31a467e4364e8a
Gougogo <1572158030@qq.com>
2023-09-19 17:31:22 +0800

    commit

--- src/com/goumi/jdbc/dao_/dao/BasicDao.java
+++ src/com/goumi/jdbc/dao_/dao/BasicDao.java
@@ -2,9 +2,13 @@ package com.goumi.jdbc.dao_.dao;
 
 import com.goumi.jdbc.dao_.utils.JDBCUtilsByDruid;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @version 1.0
@@ -12,9 +16,17 @@ import java.sql.SQLException;
  */
 public class BasicDao<T> {
     private QueryRunner qr = new QueryRunner();
+    private Connection connection = null;
 
+    //开发通用的dml方法, 针对任意的表
+
+    /**
+     *
+     * @param sql
+     * @param pars
+     * @return
+     */
     public int update(String sql, Object... pars) {
-        Connection connection = null;
 
         try {
             connection = JDBCUtilsByDruid.getConnection();
@@ -25,6 +37,59 @@ public class BasicDao<T> {
         } finally {
             JDBCUtilsByDruid.closeConnection(null, null, connection);
         }
+
     }
 
+    /**
+     *
+     * @param sql
+     * @param clazz
+     * @param pars
+     * @return
+     */
+    public List<T> queryMulti(String sql, Class<T> clazz, Object ...pars){
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return qr.query(connection, sql, new BeanListHandler<T>(clazz), pars);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.closeConnection(null, null, connection);
+        }
+    }
+
+    /**
+     *
+     * @param sql
+     * @param clazz
+     * @param pars
+     * @return
+     */
+    public T querySingle(String sql, Class<T> clazz, Object... pars){
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return qr.query(connection, sql, new BeanHandler<T>(clazz), pars);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.closeConnection(null, null, connection);
+        }
+    }
+
+    /**
+     *
+     * @param sql
+     * @param pars
+     * @return
+     */
+    public Object queryScalar(String sql, Object... pars){
+        try {
+            connection = JDBCUtilsByDruid.getConnection();
+            return qr.query(connection, sql, new ScalarHandler(), pars);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            JDBCUtilsByDruid.closeConnection(null, null, connection);
+        }
+    }
 }
